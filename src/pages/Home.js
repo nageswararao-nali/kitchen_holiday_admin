6c460cf7ea0610e0daa6153f8485e55c0ac85afc
Venkat <venky.animator@gmail.com>
2024-08-17 00:37:49 +0530

    design changes...

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,98 +16,88 @@ function Home() {
         <div className='container dashboard'>
           <div className='row'>
             <div className="col-xxl-4 col-md-6">
-                <div className="card info-card sales-card">
-                  <div className="card-body">
-                    <h5 className="card-title">All Users</h5>
-                    <div className="d-flex align-items-center" style={{justifyContent: 'center'}}>
-                      <div className="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                        <i className="bi bi-cart"></i>
-                      </div>
-                      <div className="ps-3">
-                        <h6>{totalUsers}</h6>
-                      </div>
+              <div className="card info-card sales-card widget-stat">
+                <div className="card-body p-4">
+                  <div className='media ai-icon'>
+                    <span class="me-3 bgl-primary text-primary"><svg id="icon-customers" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></span>                    
+                    <div class="media-body">
+                      <p class="mb-1"><b>All Users</b></p>
+                      <h4 class="mb-0 text-left">{totalUsers}</h4>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="col-xxl-4 col-md-6">
-                <div className="card info-card sales-card">
-                  <div className="card-body">
-                    <h5 className="card-title">Customers</h5>
-                    <div className="d-flex align-items-center" style={{justifyContent: 'center'}}>
-                      <div className="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                        <i className="bi bi-cart"></i>
-                      </div>
-                      <div className="ps-3">
-                        <h6>{totalCustomers}</h6>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-xxl-4 col-md-6">
-                <div className="card info-card sales-card">
-                  <div className="card-body">
-                    <h5 className="card-title">Delivery Boys</h5>
-                    <div className="d-flex align-items-center" style={{justifyContent: 'center'}}>
-                      <div className="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                        <i className="bi bi-cart"></i>
-                      </div>
-                      <div className="ps-3">
-                        <h6>{totalDrivers}</h6>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-          </div>
-          <div className='row'>
+            </div>
             <div className="col-xxl-4 col-md-6">
-                <div className="card info-card sales-card">
-                  <div className="card-body">
-                    <h5 className="card-title">All Orders</h5>
-                    <div className="d-flex align-items-center" style={{justifyContent: 'center'}}>
-                      <div className="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                        <i className="bi bi-cart"></i>
-                      </div>
-                      <div className="ps-3">
-                        <h6>145</h6>
-                      </div>
+              <div className="card info-card sales-card widget-stat">
+                <div className="card-body p-4">
+                  <div className='media ai-icon'>
+                    <span class="me-3 bgl-warning text-warning"><svg id="icon-customers" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></span>                    
+                    <div class="media-body">
+                      <p class="mb-1"><b>Customers</b></p>
+                      <h4 class="mb-0 text-left">{totalCustomers}</h4>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="col-xxl-4 col-md-6">
-                <div className="card info-card sales-card">
-                  <div className="card-body">
-                    <h5 className="card-title">Orders Pickedup</h5>
-                    <div className="d-flex align-items-center" style={{justifyContent: 'center'}}>
-                      <div className="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                        <i className="bi bi-cart"></i>
-                      </div>
-                      <div className="ps-3">
-                        <h6>145</h6>
-                      </div>
+            </div>
+            <div className="col-xxl-4 col-md-6">
+              <div className="card info-card sales-card widget-stat">
+                <div className="card-body p-4">
+                  <div className='media ai-icon'>
+                    <span class="me-3 bgl-danger text-danger"><svg id="icon-customers" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></span>                    
+                    <div class="media-body">
+                      <p class="mb-1"><b>Delivery Boys</b></p>
+                      <h4 class="mb-0 text-left">{totalDrivers}</h4>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="col-xxl-4 col-md-6">
-                <div className="card info-card sales-card">
-                  <div className="card-body">
-                    <h5 className="card-title">Orders Deliverd</h5>
-                    <div className="d-flex align-items-center" style={{justifyContent: 'center'}}>
-                      <div className="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                        <i className="bi bi-cart"></i>
-                      </div>
-                      <div className="ps-3">
-                        <h6>145</h6>
-                      </div>
+            </div>
+            <div className="col-xxl-4 col-md-6">
+              <div className="card info-card sales-card widget-stat">
+                <div className="card-body p-4">
+                  <div className='media ai-icon'>
+                    <span class="me-3 bgl-success text-success"><svg id="icon-customers" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></span>                    
+                    <div class="media-body">
+                      <p class="mb-1"><b>All Orders</b></p>
+                      <h4 class="mb-0 text-left">145</h4>
                     </div>
                   </div>
                 </div>
               </div>
+            </div>
+            <div className="col-xxl-4 col-md-6">
+              <div className="card info-card sales-card widget-stat">
+                <div className="card-body p-4">
+                  <div className='media ai-icon'>
+                    <span class="me-3 bgl-primary text-primary"><svg id="icon-customers" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></span>                    
+                    <div class="media-body">
+                      <p class="mb-1"><b>Orders Pickedup</b></p>
+                      <h4 class="mb-0 text-left">145</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xxl-4 col-md-6">
+              <div className="card info-card sales-card widget-stat">
+                <div className="card-body p-4">
+                  <div className='media ai-icon'>
+                    <span class="me-3 bgl-primary text-primary"><svg id="icon-customers" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></span>                    
+                    <div class="media-body">
+                      <p class="mb-1"><b>Orders Deliverd</b></p>
+                      <h4 class="mb-0 text-left">145</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            
+             
           </div>
+          
         </div>
     
   );
